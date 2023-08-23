fa5d29f58b450722196d86872f7062a391677be3
Ryan The <ryan.the.2006@gmail.com>
2023-08-23 22:01:56 +0800

    Add more data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dav-airline-delays
+
+## Data Sources
+
+| File Name | Data Obtained | Source |
+| -------------------------- | ------------- | ------------- |
+| `airports.csv` | Airport Code, Airport Airflow (measured per seats), Airport Location | https://datacatalog.worldbank.org/search/dataset/0038117/Global-Airports |
+| `flights.csv` | Flight Delays | https://www.kaggle.com/datasets/giovamata/airlinedelaycauses |
--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import sklearn
 
-flights = pd.read_csv("./Flights.csv")
-airports = pd.read_csv("./Airports.csv")
+flights = pd.read_csv("./data/flights.csv")
+airports = pd.read_csv("./data/airports.csv")
 
 pd.set_option('display.max_columns', None)
 print(flights.shape)
 print(flights.loc[:, "ArrDelay"].describe(include="all"))
 
 
-# print(flights.head(10))
+print(flights.head(10))
 
-# sns.catplot(x="ArrDelay",
-#             y="Origin",
-#             kind="strip",
-#             data=flights,
-#             order=flights['Origin'].value_counts().index,
-#             height=9,
-#             aspect=1,
-#             palette='coolwarm_r')
+sns.catplot(x="ArrDelay",
+            y="Origin",
+            kind="strip",
+            data=flights,
+            order=flights['Origin'].value_counts().index,
+            height=9,
+            aspect=1,
+            palette='coolwarm_r')
 
-# plt.show()
+plt.show()
